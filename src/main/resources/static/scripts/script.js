034c9e404b45b0bf057b82c1a3870021a41b78da
David <davidbazanticse@gmail.com>
2022-12-23 23:46:04 +0000

    #3. Image previewer done, To-do: convert picture frame to square

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -12,3 +12,26 @@ var loadFile = function (event) {
     var image = document.getElementById('output');
     image.src = URL.createObjectURL(event.target.files[0])
 };
+
+const inpFile = document.getElementById("inpFile");
+const previewContainer = document.getElementById("imagePreview");
+const previewImage = document.querySelector(".image-preview__image");
+const previewDefaultText = document.querySelector(".image-preview__default-text");
+
+inpFile.addEventListener("change", function () {
+    const file = this.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        previewDefaultText.style.display = "none";
+        previewImage.style.display = "block";
+
+        reader.addEventListener("load", function () {
+            console.log(this);
+            previewImage.setAttribute("src", this.result);
+        });
+
+        reader.readAsDataURL(file);
+    }
+});
