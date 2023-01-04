a85e9ac3c97f9844328f904331eb234cf45db762
David <davidbazanticse@gmail.com>
2023-01-04 14:31:09 +0000

    #7. Now comment form can hide or show when click on button. Add class to those form using the image previewer. To-do: style comment section

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -9,40 +9,42 @@ function hideMenu() {
 }
 
 // --- Image Previewer ---
-var loadFile = function (event) {
-    var image = document.getElementById('output');
-    image.src = URL.createObjectURL(event.target.files[0])
-};
+if (document.querySelector('form').classList.contains ('form-with-previewer')) {
+    var loadFile = function (event) {
+        var image = document.getElementById('output');
+        image.src = URL.createObjectURL(event.target.files[0])
+    };
 
-const inpFile = document.getElementById("inpFile");
-const previewContainer = document.getElementById("imagePreview");
-const previewImage = document.querySelector(".image-preview__image");
-const previewDefaultText = document.querySelector(".image-preview__default-text");
+    const inpFile = document.getElementById("inpFile");
+    const previewContainer = document.getElementById("imagePreview");
+    const previewImage = document.querySelector(".image-preview__image");
+    const previewDefaultText = document.querySelector(".image-preview__default-text");
 
-inpFile.addEventListener("change", function () {
-    const file = this.files[0];
+    inpFile.addEventListener("change", function () {
+        const file = this.files[0];
 
-    if (file) {
-        const reader = new FileReader();
+        if (file) {
+            const reader = new FileReader();
 
-        previewDefaultText.style.display = "none";
-        previewImage.style.display = "block";
+            previewDefaultText.style.display = "none";
+            previewImage.style.display = "block";
 
-        reader.addEventListener("load", function () {
-            console.log(this);
-            previewImage.setAttribute("src", this.result);
-        });
+            reader.addEventListener("load", function () {
+                console.log(this);
+                previewImage.setAttribute("src", this.result);
+            });
 
-        reader.readAsDataURL(file);
-    }
-});
+            reader.readAsDataURL(file);
+        }
+    });
+}
 // ------------------
 // --- Show/Hide Form ---
 
-const btn = document.getElementById('btn_comment')
+const btn = document.getElementById('btn-comment')
 
 btn.addEventListener('click', () => {
-    const form = document.getElementById('form_comment');
+    const form = document.getElementById('form-comment');
 
     if (form.style.display === 'none') {
         form.style.display = 'block';
