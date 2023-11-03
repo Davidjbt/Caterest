679ef1b6540538735d00ffa6886dd3221f1e675c
Davidjbt <101603373+Davidjbt@users.noreply.github.com>
2023-11-03 21:02:42 +0000

    Add function to find all students that contain query/displayName

--- src/main/java/com/david/caterest/controller/UserController.java
+++ src/main/java/com/david/caterest/controller/UserController.java
@@ -14,8 +14,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
 
@@ -32,12 +34,18 @@ public class UserController {
         userService.renderProfilePicture(userId, response);
     }
 
-    @GetMapping("/user/new")
+    @GetMapping("/find")
+    public List<UserProfileDto> findMatchingUsers(@RequestParam String query) {
+        return userService.findMatchingUsers(query);
+    }
+
+
+    @GetMapping("/new")
     public UserSignUpDto newUser() {
         return new UserSignUpDto();
     }
 
-    @GetMapping("/users/list")
+    @GetMapping("/list")
     public String getUsersList(Model model) {
         model.addAttribute("users", userService.findAll());
 
@@ -56,7 +64,7 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
-    @GetMapping("/user/profile/{displayName}")
+    @GetMapping("/profile/{displayName}")
     public UserProfileDto getUserProfileDetails(@PathVariable String displayName) {
         return userService.findUserProfileDetailsByDisplayName(displayName);
     }
--- src/main/java/com/david/caterest/repository/UserRepository.java
+++ src/main/java/com/david/caterest/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.david.caterest.repository;
 
 import com.david.caterest.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.lang.NonNull;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByDisplayNameContainsIgnoreCase(@NonNull String displayName);
     Optional<User> findByDisplayNameAndPassword(String username, String password);
     Optional<User> findByDisplayName(String username);
     Optional<User> findByEmail(String email);
